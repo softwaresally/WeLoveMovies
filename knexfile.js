fa131f0d8db9ec75d6fd169e6c9987add982d379
Sally Morris <105070505+softwaresally@users.noreply.github.com>
2022-12-07 12:09:00 -0500

    changed DATABASE_URL in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://vbnincyb:6M_js8wvxgs8sw-p853trAOoOUgkJ3z_@castor.db.elephantsql.com/vbnincyb",
 } = process.env;
 
 module.exports = {
